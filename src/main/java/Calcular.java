1576d9e883cf3f6ccb774066d8849bf46a2df2d7
OlmosLautaroGaston <lautaolmoss@gmail.com>
2019-05-10 20:31:13 -0300

    PodemosLeerPuntosCartesianosYmedirlos

--- src/main/java/Calcular.java
+++ src/main/java/Calcular.java
@@ -2,21 +2,15 @@ import javax.swing.*;
 
 public class Calcular {
     public static void main(String[] args) {
-        double x1,x2,y1,y2;
 
-        x1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese x del punto A:"));
-
-
-        y1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese y del punto A:"));
-
-
-        x2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese x del punto B:"));
+        Punto puntos = new Punto();
+        double x1 = puntos.getX1();
+        double x2 = puntos.getX2();
+        double y1 = puntos.getY1();
+        double y2 = puntos.getY2();
+        double hipotenusa = puntos.calcularHipotenusa();
 
 
-        y2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese y del punto B:"));
-        double d;
-
-       d = Math.hypot(x2-x1, y2-y1);
-        JOptionPane.showMessageDialog(null, "La distancia entre los puntos \nA("+x1+","+y1+") y B("+x2+","+y2+") es ="+d);
+        JOptionPane.showMessageDialog(null, "La distancia entre los puntos \nA("+x1+","+y1+") y B("+x2+","+y2+") es ="+hipotenusa);
     }
 }
--- src/main/java/Punto.java
+++ src/main/java/Punto.java
@@ -1,32 +1,53 @@
+import javax.swing.*;
+
 public class Punto {
-    private int x;
-    private int y;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private double hipotenusa;
 
-    public Punto(int x, int y) {
-        this.x=x;
-        this.y=y;
-    }
-
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
+    public Punto() {
+        this.setX1(x1);
+        this.setY1(y1);
+        this.setX2(x2);
+        this.setY2(y2);
     }
 
-    public void setX(int x) {
-        this.x=x;
+
+    public double getX1() {
+        return x1;
     }
-    public void setY(int y) {
-        this.y=y;
+
+    public void setX1(double x1) {
+        this.x1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese x del punto A:"));
     }
-    public int calcularDistanciaDesde(Punto parametros) {
-        int cateto1 = x = parametros.getX();
-        int cateto2 = y = parametros.getY();
-        int hipotenusa = (int) Math.sqrt(cateto1*cateto1 + cateto2*cateto2);
+
+    public double getY1() {
+        return y1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese y del punto A:"));
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese x del punto B:"));
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese y del punto B:"));
+    }
+    public double calcularHipotenusa() {
+        hipotenusa = Math.hypot(x2-x1, y2-y1);
         return hipotenusa;
-    }
-    public String toString(){
-        return "(x= " + getX() + ", y= " + getY() +"  )";
     }
 }
