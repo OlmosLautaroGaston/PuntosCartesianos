fb26c987bd91287906a91f554a0f5024ea108a98
OlmosLautaroGaston <lautaolmoss@gmail.com>
2019-05-24 21:35:31 -0300

    Avances, Comprobar hipotenusa y medidas.. terminar ex1

--- src/main/java/Calcular.java
+++ src/main/java/Calcular.java
@@ -4,39 +4,13 @@ import java.util.Scanner;
 
 public class Calcular {
     public static void main(String[] args) {
-        int [][] arreglo = new int[20][2];
-        Lector lector = new Lector();
+        /**Generamos un scanner que nos permite leer el numero de punto que deseamos crear**/
         Scanner scaner=new Scanner(System.in);
-        Punto punto = new Punto();
-        lector.contarVueltas();
-        System.out.println("Introduzca la cantidad de puntos a evaluar");
-        int e = scaner.nextInt();
-        int x;
-        int y;
-        for (x=0;x<e;x++){
-            for (y=0;y<2;y++) {
-                System.out.println("dijite los elementos ["+x+"] y de ["+y+"]");
-                arreglo[x][y]=scaner.nextInt();
-            }
-        }
-        int [][] arrayacum = new int[20][2];
-        for (x=0; x < arreglo.length; x++) {
-            for (y=0; y < arreglo[x].length; y++) {
-                if(y==0){
-                    arrayacum[x][y] = arreglo[x][y]-arreglo[x][y+1];
-                    punto.setX1(arrayacum[x][y]);
-                }else{
-                    arrayacum[x][y] = arreglo[x][y]-arreglo[x+1][y];
-                    punto.setY1(arrayacum[x][y]);
-                }
-            }
-        }
-        for (int[]fila:arreglo){
-            System.out.println();
-            for (int z: fila){
-                System.out.print(z +"");
-            }
-        }
-        //JOptionPane.showMessageDialog(null, "La distancia entre los puntos \nA("+x1+","+y1+") y B("+x2+","+y2+") es ="+hipotenusa);
+        System.out.println("Cuantos puntos desea crear? ");
+        int cantPoint = scaner.nextInt();
+        /**Instanciamos la clase Punto, en su controlador nos pide la cantidad de Puntos que deseamos crear**/
+            Punto point = new Punto(cantPoint);
+            point.generateXY();
+            point.calculateDistanceXY();
     }
 }
--- src/main/java/Punto.java
+++ src/main/java/Punto.java
@@ -1,58 +1,59 @@
+import java.util.Scanner;
+
 public class Punto {
-    private double x1;
-    private double y1;
-    private double x2;
-    private double y2;
-    private double hipotenusa;
+    private int x;
+    private int y;
+    private int[] arrx;
+    private int[] arry;
+    private int firstEntrance;
+    private int counter=1;
+    int hypo;
 
-    public Punto() {
-        this.setX1(getX1());
-        this.setX2(getX2());
-        this.setY1(getY1());
-        this.setY2(getY2());
+    Scanner scaner = new Scanner(System.in);
+
+    public Punto(int firstEntrance) {
+    this.firstEntrance=firstEntrance;
+    arrx = new int[firstEntrance];
+    arry = new int[firstEntrance];
     }
 
+    public int generateXY(){
+        for(int i=0;i<firstEntrance;i++){
 
-    public double calcularHipotenusa() {
-        hipotenusa = Math.hypot(getX2() - getX1(), getY2() - getY1());
-        return hipotenusa;
+            System.out.println("Ingrese el valor de X del punto "+counter+" : ");
+            arrx[i] = scaner.nextInt();
+            System.out.println("Ingrese el valor de Y del punto "+counter+" : ");
+            arry[i] = scaner.nextInt();
+            counter++;
+        }
+        return counter;
     }
+    public void calculateDistanceXY(){
+        for(int i=0;i<arrx.length;i++){
+            for (int j=0;j<arry.length;j++){
+                hypo = (int) Math.sqrt(Math.pow(arrx[i]+arry[i], 2) + Math.pow(arrx[j]+arry[j], 2));
 
-    public double getX1() {
-        return x1;
+                System.out.println("la distancia entre X e Y "+arrx[i]+" --- "+arry[j]+" es : "+hypo);
+            }
+        }
     }
-
-    public void setX1(double x1) {
-        this.x1 = x1;
-        System.out.println("el valor de x es"+x1);
-    }
-
-    public double getY1() {
-        return y1;
-    }
-
-    public void setY1(double y1) {
-        this.y1 = y1;
-        System.out.println("el valor de y es"+y1);
-    }
-
-    public double getX2() {
-        return x2;
-    }
-
-    public void setX2(double x2) {
-        this.x2 = x2;
-        System.out.println("el valor de x2 es"+x2);
+    public void hipoXY(){
 
     }
 
-    public double getY2() {
-        return y2;
-    }
-
-    public void setY2(double y2) {
-        this.y2 = y2;
-        System.out.println("el valor de y2 es"+y2);
-
-    }
+  /**  public void passNumtoArrXY() {
+        String str = (new Integer(entrance)).toString();
+        char[] chArr = str.toCharArray();
+        if (counter % 2 == 0) {
+            for (int i = 0; i < chArr.length; i++) {
+                arry[i] = Character.getNumericValue(chArr[i]);
+                System.out.println("El numero se almaceno en el arreglo x: " + arry[i]);
+            }
+        } else {
+            for (int i = 0; i < chArr.length; i++) {
+                arrx[i] = Character.getNumericValue(chArr[i]);
+                System.out.println("El numero se almaceno en el arreglo y: " + arrx[i]);
+            }
+        }
+    }**/
 }
