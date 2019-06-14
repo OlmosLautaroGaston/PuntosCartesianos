956a350a5a1656bde0d0c0a860ade5f8d7f8c8e3
OlmosLautaroGaston <lautaolmoss@gmail.com>
2019-06-14 20:40:28 -0300

    Version 2.0 finished

--- src/main/java/Punto.java
+++ src/main/java/Punto.java
@@ -1,8 +1,9 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Punto {
-    private int x;
-    private int y;
+    private int init;
+    private int end;
     private int[] arrx;
     private int[] arry;
     private int[] cont;
@@ -11,6 +12,7 @@ public class Punto {
     private double numMinimo = 1000000;
 
     Scanner scaner = new Scanner(System.in);
+    DecimalFormat df = new DecimalFormat("#.00");
 
     public Punto(int firstEntrance) {
         this.firstEntrance = firstEntrance;
@@ -22,30 +24,30 @@ public class Punto {
     public void generateXY() {
         for (int i = 0; i < firstEntrance; i++) {
             cont[i] = i + 1;
-            System.out.println("Ingrese el valor de X del punto " + cont[i] + " : ");
+            System.out.println("Ingrese el valor de X del Punto < " + cont[i] + " >  ");
             arrx[i] = scaner.nextInt();
-            System.out.println("Ingrese el valor de Y del punto " + cont[i] + " : ");
+            System.out.println("Ingrese el valor de Y del Punto < " + cont[i] + " >  ");
             arry[i] = scaner.nextInt();
         }
     }
 
     public void calculateDistanceXY() {
-        for (int i = 0; i < arrx.length; i++) {
-            for (int j = 0; j < arry.length; j++) {
-                if (cont[i] != cont[j]) {
+        for (int i = 0; i < arrx.length-1; i++) {
+            for (int j = i+1; j < arry.length; j++) {
                     hypo = Math.sqrt(Math.pow(arrx[i] - arrx[j], 2) + Math.pow(arry[i] - arry[j], 2));
                     calculateDistanceMIN(hypo,cont[j],cont[i]);
-                   // System.out.println("la distancia entre el punto " + cont[j] + " y " + cont[i] + " es :" + hypo);
-                }
+                    System.out.println("la distancia entre el punto " + cont[i] + " y " + cont[j] + " es < " + df.format(hypo)+" >");
+
             }
         }
-        System.out.println("La distancia más corta es < "+numMinimo+" > entre los puntos < "+y+" > y < "+x+" >");
+        System.out.println("");
+        System.out.println("La distancia más corta es < "+df.format(numMinimo)+" > entre los puntos < "+end+" > y < "+ init +" >");
     }
     private void calculateDistanceMIN(double hypo, int j,int i){
         if (numMinimo>hypo){
             numMinimo = hypo;
-            x = j;
-            y = i;
+            init = j;
+            end = i;
         }
 
     }
